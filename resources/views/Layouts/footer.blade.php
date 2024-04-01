4dee8491119748037def0f24d336b69c7c37efbd
= <dipenp133@gmail.com>
2024-04-02 02:52:38 +0530

    home page done

--- resources/views/Layouts/footer.blade.php
+++ resources/views/Layouts/footer.blade.php
@@ -1,3 +1,73 @@
-<footer>
-footer
+<!-- Footer -->
+<footer class="bg-dark footer-top">
+    <div class="container pt-md-10 pt-20 pb-5">
+        <div class="row gy-6 mb-6 gx-xl-12">
+            <div class=" col-lg-3">
+                <div class="mb-4">
+                    <a href="#" target="_blank">
+                        <img class="mb-4" width="207" src="{{ asset('frontend/images/logo/logo-light.svg') }}"
+                            alt="Therapy Zone">
+                    </a>
+                    <p class="text-white text-semibold text-sm mb-4">Therapy Zone: Your path to well-being
+                        begins here. Discover services and insights for a happier, healthier you.</p>
+                    <a class="d-block mb-2 text-gray-300 text-primary-hover" href="#">
+                        <i class="bi bi-geo-alt me-2 text-primary"></i>Nikol Ahmedabad Gujrat 382350</a>
+                    <a class="d-block mb-2 text-gray-300 text-primary-hover" href="#">
+                        <i class="bi bi-envelope me-2 text-primary"></i> support@doctors.com</a>
+                    <a class="d-block mb-2 text-gray-300 text-primary-hover" href="#">
+                        <i class="bi bi-telephone me-2 text-primary"></i> +91 12345 67890</a>
+
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <p class="text-primary font-bold mb-4">Company</p>
+                <a class="d-block mb-2 text-gray-300 text-primary-hover" href="#">Home</a>
+                <a class="d-block mb-2 text-gray-300 text-primary-hover" href="#">About Us</a>
+                <a class="d-block mb-2 text-gray-300 text-primary-hover" href="#">Services</a>
+                <a class="d-block mb-2 text-gray-300 text-primary-hover" href="#">Appointments</a>
+                <a class="d-block text-gray-300 text-primary-hover " href="#">Contact Us</a>
+            </div>
+            <div class="col-lg-4">
+                <p class="text-primary font-bold mb-4">Timing</p>
+                <a class="d-block mb-2 text-gray-300 text-primary-hover" href="#">Sunday 10:30 AM to 09:30 PM</a>
+                <a class="d-block mb-2 text-gray-300 text-primary-hover" href="#">Monday 10:30 AM to 09:30 PM</a>
+                <a class="d-block mb-2 text-gray-300 text-primary-hover" href="#">Tuesday 10:30 AM to 09:30 PM</a>
+                <a class="d-block mb-2 text-gray-300 text-primary-hover" href="#">Wednesday 10:30 AM to 09:30 PM</a>
+                <a class="d-block mb-2 text-gray-300 text-primary-hover" href="#">Thursday 10:30 AM to 09:30 PM</a>
+                <a class="d-block mb-2 text-gray-300 text-primary-hover" href="#">Friday 10:30 AM to 09:30 PM</a>
+                <a class="d-block text-gray-300 text-primary-hover" href="#">Saturday 10:30 AM to 09:30 PM</a>
+            </div>
+            <div class="col-lg-3">
+                <p class="text-primary font-bold mb-4">Our Social Networks</p>
+                <div class="footer-social-links d-flex gap-3">
+                    <a href="#" class="w-10 h-10 rounded-circle bg-light bg-primary-hover hstack justify-content-center"
+                        title="Follow Us On Facebook" target="_blank">
+                        <i class="bi bi-facebook text-black"></i>
+                    </a>
+                    <a href="#" class="w-10 h-10 rounded-circle bg-light bg-primary-hover hstack justify-content-center"
+                        title="Follow Us On instagram" target="_blank">
+                        <i class="bi bi-instagram text-black"></i>
+                    </a>
+                    <a href="#" class="w-10 h-10 rounded-circle bg-light bg-primary-hover hstack justify-content-center"
+                        title="Follow Us On linkedin" target="_blank">
+                        <i class="bi bi-linkedin text-black"></i>
+                    </a>
+                    <a href="#" class="w-10 h-10 rounded-circle bg-light bg-primary-hover hstack justify-content-center"
+                        title="Follow Us On twitter" target="_blank">
+                        <i class="bi bi-twitter-x text-black"></i>
+                    </a>
+                    <a href="#" class="w-10 h-10 rounded-circle bg-light bg-primary-hover hstack justify-content-center"
+                        target="_blank">
+                        <i class="bi bi-skype text-black"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
+<footer class="bg-dark footer-bottom">
+    <div class="container border-top border-gray-800">
+        <p class="text-sm text-gray-500 py-4">© {{ date('Y') }} Therapy Zone. All rights reserved.</p>
+    </div>
+</footer>
+<!-- Footer -->
--- resources/views/Layouts/header.blade.php
+++ resources/views/Layouts/header.blade.php
@@ -1,7 +1,7 @@
 <header class="sticky-top backdrop-blur-5 bg-light bg-opacity-80 border-bottom">
     <div class="container">
         <nav class="tv-header navbar navbar-expand-lg py-3 px-0">
-            <a class="navbar-brand py-0" href="https://www.techvoot.com">
+            <a class="navbar-brand py-0" href="#">
                 <!-- Regular Logo -->
                 <img src="{{ asset('frontend/images/logo/logo.svg') }}" alt="therapy zone" width="205">
             </a>
@@ -13,22 +13,21 @@
             <div class="collapse navbar-collapse custome-collapse gap-6" id="navbarMenu">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link py-xl-2 px-3" href="https://www.techvoot.com/portfolio"><span>Home</span></a>
+                        <a class="nav-link py-xl-2 px-3" href="#"><span>Home</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link py-xl-2 px-3" href="https://www.techvoot.com/portfolio"><span>About
+                        <a class="nav-link py-xl-2 px-3" href="#"><span>About
                                 Us</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link py-xl-2 px-3"
-                            href="https://www.techvoot.com/portfolio"><span>Services</span></a>
+                        <a class="nav-link py-xl-2 px-3" href="#"><span>Services</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link py-xl-2 px-3" href="https://www.techvoot.com/portfolio"><span>Contact
+                        <a class="nav-link py-xl-2 px-3" href="#"><span>Contact
                                 Us</span></a>
                     </li>
                 </ul>
-                <a class="btn btn-sm btn-dark px-6" role="button" href="https://www.techvoot.com/free-consultation">BOOK
+                <a class="btn btn-sm btn-dark px-6" role="button" href="#">BOOK
                     NOW</a>
             </div>
         </nav>
