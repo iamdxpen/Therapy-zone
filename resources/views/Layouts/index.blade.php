95091240966f895c4ffb3a2ef593bf01f9c171df
= <dipenp133@gmail.com>
2024-04-04 05:54:44 +0530

    test

--- resources/views/Layouts/index.blade.php
+++ resources/views/Layouts/index.blade.php
@@ -4,11 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://unpkg.com/@webpixels/css@1.2.6/dist/index.css" rel="stylesheet">
     <!-- <link href="{{ asset('frontend/css/app.min.css?ver=')}}{{env('CDN_VERSION') }}" rel="stylesheet">> -->
-    <!-- slider -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link href="https://unpkg.com/@webpixels/css@1.2.6/dist/index.css" rel="stylesheet">
+    <!-- slider -->
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Therapy zone</title>
 </head>
 
